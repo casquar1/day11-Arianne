d83d4c34b20ced18599f8a9c9eead35409ecd92e
Arianne <chiescake11@gmail.com>
2023-08-29 21:06:02 +0800

    add promise practice demo

---
+++ todo-list/promisePracticeDemo.js
@@ -0,0 +1,30 @@
+const successPromise = new Promise((resolve, rejected) => {
+    setTimeout(() => {
+        resolve("This will be printed.");
+    }, 1000);
+    setTimeout(() => {
+        rejected("This will not be printed.")
+    }, 2000);
+});
+
+successPromise.then((message) => {
+    console.log("Success: " + message);
+}).catch((message) => {
+    console.log("Failed: " + message);
+});
+
+
+const failedPromise = new Promise((resolve, rejected) => {
+    setTimeout(() => {
+        resolve("This will not be printed.");
+    }, 2000);
+    setTimeout(() => {
+        rejected("This will be printed.")
+    }, 1000);
+});
+
+failedPromise.then((message) => {
+    console.log("Success: " + message);
+}).catch((message) => {
+    console.log("Failed: " + message);
+});
