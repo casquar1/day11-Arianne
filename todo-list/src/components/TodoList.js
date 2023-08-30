d98bda0a7725aae85da8b0beab240d8854194164
Arianne <chiescake11@gmail.com>
2023-08-30 13:16:59 +0800

    refactor: re-add accidentally removed div

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -7,10 +7,12 @@ const TodoList = () => {
     const todos = useSelector((state) => state.todo.todoList);
 
     return (
-        <div className="todo-container">
-            <h1>Todo List</h1>
-            <TodoGenerator />
-            <TodoGroup todos={todos} />
+        <div className="container">
+            <div className="todo-container">
+                <h1>Todo List</h1>
+                <TodoGenerator />
+                <TodoGroup todos={todos} />
+            </div>
         </div>
     );
 }
